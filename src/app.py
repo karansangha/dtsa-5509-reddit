c7a4116914d925fd3fe2124a9d6b623e6f19113e
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2023-02-07 20:35:43 -0800

    initial commit

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return '''
+     <h1>Twitter Analyzer</h1>
+     <form action="/echo_user_input" method="POST">
+         <label for="user_input">Enter any text - </label>
+         <input name="user_input">
+         <input type="submit" value="Submit!">
+     </form>
+     '''
+
+
+@app.route("/echo_user_input", methods=["POST"])
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    return "<h1>Thank you for using the form!</h1><p>You entered: " + input_text + "</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
