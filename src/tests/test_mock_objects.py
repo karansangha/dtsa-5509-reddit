7f7c4d2eaf967412236beff88e7b9e9acc65ed10
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2023-02-15 21:41:42 -0800

    add mock tests

---
+++ src/tests/test_mock_objects.py
@@ -0,0 +1,17 @@
+from ..data_collector import collector
+from ..data_analyzer import analyzer
+
+def test_collector_authenticate(mocker):
+    mocker.patch('src.data_collector.collector.authenticate')
+    collector.get_top_posts('python')
+    collector.authenticate.assert_called_once()
+
+def test_collector_get_top_posts(mocker):
+    mocker.patch('src.data_collector.collector.get_top_posts')
+    collector.get_top_posts('python')
+    collector.get_top_posts.assert_called_once_with('python')
+
+def test_analyzer_generate_visualizations_data(mocker):
+    mocker.patch('src.data_analyzer.analyzer.generate_visualizations_data')
+    analyzer.generate_visualizations_data('python')
+    analyzer.generate_visualizations_data.assert_called_once_with('python')
