b0f070ae5daad99ddf37e3fb0dfa7329c4d34d7b
Karanvir Singh Sangha <iamkaransangha@gmail.com>
2023-02-15 17:03:52 -0800

    load the env variables

--- run_analyzer.sh
+++ run_analyzer.sh
@@ -5,5 +5,8 @@ echo "Activating virtual environment"
 source .venv/bin/activate
 pip install -r requirements.txt > /dev/null 2>&1 || echo "Error installing dependencies. Please run pip install -r requirements.txt manually"
 
+echo "Loading environment variables"
+source .env
+
 echo "Starting Data Analyzer"
 python3 src/data_analyzer/analyzer.py
